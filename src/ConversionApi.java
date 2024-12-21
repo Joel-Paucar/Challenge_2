18554bdc09199e86d7ce183c57ad95c8874e476b
Joel-Paucar <paucar.pauca@gmail.com>
2024-12-21 16:31:39 -0500

    Editando clase ConversionApi

--- src/ConversionApi.java
+++ src/ConversionApi.java
@@ -5,20 +5,20 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ConversionApi {
-    private String json;
-    public ConversionApi(String local, String conver) throws IOException, InterruptedException {
+    private HttpClient client;
+
+    public ConversionApi(){
+        client = HttpClient.newHttpClient();
+    }
+    public String LlamarApi(String local, String conver) throws IOException, InterruptedException {
         String url = "https://v6.exchangerate-api.com/v6/edafa679a9c67d9774d3772c/pair/" +
-                        local+"/"+conver;
-        HttpClient client = HttpClient.newHttpClient();
+                local+"/"+conver;
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .build();
         HttpResponse<String>response =client.send(request,
                 HttpResponse.BodyHandlers.ofString());
-        json = response.body();
-    }
-
-    public String getJson() {
+        String json = response.body();
         return json;
     }
 }
