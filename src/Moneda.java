a82f36076a75506407fd77bff37ac6bc3a1bafe7
Joel-Paucar <paucar.pauca@gmail.com>
2024-12-23 01:15:46 -0500

    Cambio de clase a record

--- src/Moneda.java
+++ src/Moneda.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.util.Date;
 
-public class Moneda {
+public record Moneda(String valorInicial,
+                     String valorFinal,
+                     String monedaBase,
+                     String monedaObjetivo,
+                     Date date) {
+
+    public String toString(){
+        return "(" + this.date + ") Se convirtio " + this.valorInicial + " de la moneda '" + this.monedaBase
+                + "' a " + this.valorFinal + " de la moneda '" + this.monedaObjetivo + "'.";
+    }
 }
