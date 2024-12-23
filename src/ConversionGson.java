75115e2ddf19124b0827a6dd3593370ae512f8d8
Joel-Paucar <paucar.pauca@gmail.com>
2024-12-23 02:24:09 -0500

    Convertiendo gson en record Moneda

--- src/ConversionGson.java
+++ src/ConversionGson.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class ConversionGson {
+    private Gson gson;
+
+    public ConversionGson(){
+        gson = new GsonBuilder().setPrettyPrinting().create();
+    }
+    public MonedaExchange LlamarGson (String json){
+        return gson.fromJson(json, MonedaExchange.class);
+    }
+
+    public Gson getGson() {
+        return gson;
+    }
 }
